0238fd7e2009d5f984d13438a9ab98c28ab02744
kellyhubert <irakellyhub@gmail.com>
2025-11-24 16:19:58 +0200

    middleware and settings are updated

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from datetime import timedelta
+import dj_database_url
 
 load_dotenv()
 
@@ -25,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!r%y)qyu-mb@7t=8k35!yx)k&lejx9$iul+l^2#b3@o_8-grg4'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-!r%y)qyu-mb@7t=8k35!yx)k&lejx9$iul+l^2#b3@o_8-grg4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
